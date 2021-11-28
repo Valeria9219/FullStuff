8089b011f0dbe8852048ce7552a5be521ea28488
Valeri <kaleriaAsh@gmail.ru>
2021-11-28 23:26:25 +0300

    commit изменения в js - задание и вывод данных о переменных

--- script.js
+++ script.js
@@ -7,3 +7,20 @@ let fullPrice;
 let adaptive;
 alert("Модальное окно - Hello World");
 console.log('Консоль - Hello World1');
+
+title = 'lesson02';
+screens = "Простые, Сложные, Интерактивные";
+screenPrice = 29;
+rollback = 15;
+fullPrice = 59999;
+adaptive = true;
+
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+console.log(screens.length);
+
+console.log("Стоимость верстки экранов " + screenPrice
+    + " рублей/долларов/гривен/юани \nСтоимость разработки сайта "
+    + fullPrice + " рублей/долларов/гривен/юани");
+console.log(fullPrice * (rollback / 100));
