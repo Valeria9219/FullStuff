7e0cce1279060b2bc19c4dbd7e7fc75b188b1881
Valeri <kaleriaAsh@gmail.ru>
2021-11-29 09:28:29 +0300

    изменение структуры кода - объявление перем и присвоение значения 1 командой

---
+++ script.js
@@ -0,0 +1,22 @@
+// js файл
+"use strict"
+
+let title = 'lesson02';
+let letscreens = "Простые, Сложные, Интерактивные";
+let screenPrice = 29;
+let rollback = 15;
+let fullPrice = 59999;
+let adaptive = true;
+
+alert("Модальное окно - Hello World");
+console.log('Консоль - Hello World1');
+
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+console.log(screens.length);
+
+console.log("Стоимость верстки экранов " + screenPrice
+    + " рублей/долларов/гривен/юани \nСтоимость разработки сайта "
+    + fullPrice + " рублей/долларов/гривен/юани");
+console.log(fullPrice * (rollback / 100));
